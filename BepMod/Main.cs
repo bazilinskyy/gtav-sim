7c206a719565c5b6347a7bc69d3e50eec14d140f
Niels Heisterkamp <niels@ineffable.nl>
2017-03-04 22:18:43 +0100

    Initial commit

---
+++ BepMod/Main.cs
@@ -0,0 +1,20 @@
+﻿using GTA;
+
+namespace BepMod
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            KeyDown += Main_KeyDown;
+        }
+
+        private void Main_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.J)
+            {
+                UI.Notify("Hello, world!");
+            }
+        }
+    }
+}
