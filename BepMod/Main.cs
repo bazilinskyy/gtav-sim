31c7c15ca4c670b590690d29fb93103f6ebdfbdf
Niels Heisterkamp <niels@ineffable.nl>
2017-03-05 00:21:09 +0100

    Show location when pressing L, spawn bicycle on J

--- BepMod/Main.cs
+++ BepMod/Main.cs
@@ -1,4 +1,6 @@
 ﻿using GTA;
+using GTA.Math;
+using GTA.Native;
 
 namespace BepMod
 {
@@ -8,12 +10,28 @@ namespace BepMod
         {
             KeyDown += Main_KeyDown;
         }
-
+        
         private void Main_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            if (e.KeyCode == System.Windows.Forms.Keys.J)
+            if (e.KeyCode == System.Windows.Forms.Keys.L)
             {
-                UI.Notify("Hello, world!");
+                UI.ShowSubtitle(
+                    "Position: " + Game.Player.Character.Position.ToString() + 
+                    ", heading: " + Game.Player.Character.Heading.ToString());
+            }
+            else if (e.KeyCode == System.Windows.Forms.Keys.J)
+            {
+                // 5 meters in front of the player
+                Vector3 position = Game.Player.Character.GetOffsetInWorldCoords(new Vector3(0, 5, 0));
+
+                // At 90 degrees to the players heading
+                float heading = Game.Player.Character.Heading - 90;
+
+                Vehicle vehicle = World.CreateVehicle(VehicleHash.TriBike3, position, heading);
+                
+                vehicle.PlaceOnGround();
+
+                UI.ShowSubtitle("Your bicycle has arrived!");
             }
         }
     }
