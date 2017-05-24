01b05858f09294cda265ed46c19bc51d11e81745
Niels Heisterkamp <niels@ineffable.nl>
2017-05-24 11:18:51 +0200

    Add gps sound

--- BepMod/Experiment/GPS.cs
+++ BepMod/Experiment/GPS.cs
@@ -10,14 +10,44 @@ namespace BepMod.Experiment
 {
     class GPS
     {
+        public enum Sound
+        {
+            NUM_10,
+            NUM_100,
+            NUM_20,
+            NUM_200,
+            NUM_300,
+            NUM_400,
+            NUM_50,
+            NUM_500,
+            NUM_800,
+            BING_BONG,
+            CALCULATINROUTE,
+            EXITFREEWAY,
+            HIGHLIGHTEDROUTE,
+            IN,
+            JOINFREEWAY,
+            KEEP,
+            LEFT,
+            RECALCULATING,
+            RIGHT,
+            STRAIGHTAHEAD,
+            TONE,
+            TURN,
+            UHAVEARRIVED,
+            UTURN,
+            YARDS
+        };
+
         public SoundPlayer SoundLeft = new SoundPlayer();
         public SoundPlayer SoundRight = new SoundPlayer();
         public SoundPlayer SoundStraight = new SoundPlayer();
+        public SoundPlayer SoundUHaveArived = new SoundPlayer();
+
         public string audioBase = ".\\scripts\\GPS\\Audio\\snoop_dogg\\";
 
         public GPS()
         {
-
             SoundLeft.SoundLocation = audioBase + "LEFT.WAV";
             SoundLeft.Load();
 
@@ -27,6 +57,8 @@ namespace BepMod.Experiment
             SoundStraight.SoundLocation = audioBase + "STRAIGHTAHEAD.WAV";
             SoundStraight.Load();
 
+            SoundUHaveArived.SoundLocation = audioBase + "UHAVEARRIVED.WAV";
+            SoundUHaveArived.Load();
         }
     }
 }
